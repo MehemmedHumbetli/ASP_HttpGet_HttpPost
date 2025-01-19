2b6b4aae35a28f67308d60623cf229fcb5b3c50f
Humbe_li59 <Humbe_li59@STHQ0128-15>
2025-01-19 15:03:11 +0400

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace WebApplication2.Controllers
+{
+    public string path = "User.json";
+
+    public class HomeController : Controller
+    {
+        public IActionResult ViewResult()
+        {
+            return View();
+        }
+    }
+}
