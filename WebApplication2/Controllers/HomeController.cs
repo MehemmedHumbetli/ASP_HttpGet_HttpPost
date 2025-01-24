7c4041c8a5e1bf936f27408ea351f358e4fb485d
MehemmedLinux <mehemedhumbetov1@example.com>
2025-01-24 21:26:36 +0400

    Delete qaldi

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using WebApplication2.Entites;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using WebApplication2.Models;
+using System.Linq;
 
 namespace WebApplication2.Controllers
 {
@@ -17,6 +18,8 @@ namespace WebApplication2.Controllers
             _hostingEnvironment = hostingEnvironment;
         }
 
+        //Index ve Details eyni isi gorur sadece biri adi ve soyadi view eliyir digeri ise full propertileri ile view eliyir
+        #region  
         public IActionResult Index()
         {
             var users = GetUsersFromJson();
@@ -36,7 +39,10 @@ namespace WebApplication2.Controllers
             };
             return View(viewModel);
         }
+        #endregion
 
+        //.Jsondan datani elde edirik (Oxuyuruq)
+        #region
         private List<User> GetUsersFromJson()
         {
             var rootPath = _hostingEnvironment.ContentRootPath;
@@ -47,14 +53,17 @@ namespace WebApplication2.Controllers
                 return JsonConvert.DeserializeObject<List<User>>(json);
             }
         }
+        #endregion
 
         [HttpGet]
-        public IActionResult Add() 
+        public IActionResult Add()
         {
             var vm = new UserAddViewModel();
             return View(vm);
         }
 
+        //View da input-a daxil olunan datani post vasitesi ile elde edirik
+        #region
         [HttpPost]
         public IActionResult Add(UserAddViewModel viewModel)
         {
@@ -72,17 +81,39 @@ namespace WebApplication2.Controllers
                 var users = GetUsersFromJson();
                 users.Add(newUser);
 
-                var rootPath = _hostingEnvironment.ContentRootPath;
-                var filePath = Path.Combine(rootPath, "Helpers", "User.json");
-                var json = JsonConvert.SerializeObject(users, Formatting.Indented);
-                System.IO.File.WriteAllText(filePath, json);
+                WriteToJson(users);
 
                 return RedirectToAction("Index");
             }
 
             return View(viewModel);
         }
+        #endregion
 
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var users = GetUsersFromJson();
 
+            var userToRemove = users.FirstOrDefault(u => u.Id == id);
+
+            users.Remove(userToRemove);
+
+            WriteToJson(users);
+            return RedirectToAction("Index");
+        }
+
+        // Datani .json-a yazan method
+        #region
+        private void WriteToJson(List<User> users)
+        {
+            var rootPath = _hostingEnvironment.ContentRootPath;
+            var filePath = Path.Combine(rootPath, "Helpers", "User.json");
+
+            var json = JsonConvert.SerializeObject(users, Formatting.Indented);
+
+            System.IO.File.WriteAllText(filePath, json);
+        }
+        #endregion
     }
 }
